88c3d3cfb121c898376a1857910758f4111d5a76
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2020-10-16 11:30:04 +0200

    Back to development: 1.4.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def project_path(*names):
 
 setup(
     name='sphinx_zon_theme',
-    version='1.4.1',
+    version='1.4.2.dev0',
 
     install_requires=[
         'sphinx_rtd_theme >= 0.5.0.dev0',
